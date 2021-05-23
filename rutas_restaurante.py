aaa298f4a9783b4cc7bd0be5b622ec777c7a0977
Erick Ruiz <eruisacosta@gmail.com>
2021-05-23 12:44:16 -0500

    Microservicio restaurante terminado

--- rutas_restaurante.py
+++ rutas_restaurante.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, sessions, redirect, url_for
+from flask.helpers import flash
+import requests
+import json
+
 
 rutas_restaurante = Blueprint('rutas_restaurante', __name__, template_folder='templates', static_folder='static')
 
@@ -7,35 +11,82 @@ rutas_restaurante = Blueprint('rutas_restaurante', __name__, template_folder='te
 
 @rutas_restaurante.route('/admin', methods=['GET', 'POST'])
 def admin_page():
-    return render_template('admin-page.html')
+    if 'user_logged' in session:
+        return render_template('admin-page.html')
+    return redirect (url_for('rutas_sesiones.login_restaurante'))
 
 
 # Pagina inicio de administracion de los restaurantes
 
 @rutas_restaurante.route('/cambiar-informacion', methods=['GET', 'POST'])
 def cambiar_informacion():
-    if (request.method == 'GET'):
-        data = {
-            "id": 13456,
-            "nombre_restaurante": "El Baluarte",
-            "Color": "#FFC300",
-            "logo": "https://baluartesfj.com/wp-content/uploads/2018/05/logo_home_2.png",
-            "chef_principal": "Maria Fernandez",
-            "especialidad": "Comida Caribeña"
-        }
-        context = {'restaurante': data}
-        return render_template('cambiar-informacion-restaurante.html', **context)
+    if 'user_logged' in session:
+        if (request.method == 'GET'):
+            restaurante = {'id_restaurante': session['user_logged']}
+            request_microservicio_restaurante = requests.get('http://localhost:7070/api/HttpTrigger3', json=restaurante)
+            context = {'restaurante': json.loads(request_microservicio_restaurante.text)}
+            return render_template('cambiar-informacion-restaurante.html', **context)
+        if (request.method == 'POST'):
+            try:
+                restaurante ={
+                "id_restaurante": session['user_logged'],
+                "nombre_restaurante": request.form['nombre-restaurante'],
+                "color": request.form['colores'],
+                "logo": request.form['logo'],
+                "chef_principal": request.form['chef'],
+                "especialidad": request.form['especialidad']
+                }
+                request_microservicio_restaurante = requests.put('http://localhost:7070/api/HttpTrigger2', json=restaurante)
+                if(request_microservicio_restaurante.status_code==200):
+                    flash('Informacion actualizada con exito')
+                    context = {'status_code': 200}
+                    return redirect(url_for('rutas_restaurante.cambiar_informacion'))
+                else:
+                    flash('Algo salio mal o los datos no estan completos, vuelva a intentarlo')
+                    context = {'status_code': 500}
+                    return redirect(url_for('rutas_restaurante.cambiar_informacion'))
+            except:
+                flash('Algo salio mal, vuelvalo a intentar')
+                context = {'status_code': 500}
+                return render_template('cambiar-informacion-restaurante.html', **context)
+    return redirect (url_for('rutas_sesiones.login_restaurante'))
 
 
 # Pagina de administracion de los platos y la carta
 
 @rutas_restaurante.route('/administrar-carta', methods=['GET', 'POST'])
 def administrar_carta():
-    return render_template('administrar-carta.html')
+    if 'user_logged' in session:
+        return render_template('administrar-carta.html')
+    return redirect (url_for('rutas_sesiones.login_restaurante'))
 
 
 # Pagina de cambio de contraseña
 
 @rutas_restaurante.route('/cambiar-contraseña', methods=['GET', 'POST'])
 def cambiar_password():
-    return render_template('cambiar-password.html')
+    if 'user_logged' in session:
+        if(request.method=='GET'):
+            return render_template('cambiar-password.html')
+        elif(request.method=='POST'):
+            try:
+                change_password = {
+                    'old_password': request.form['password1'],
+                    'new_password': request.form['password2'],
+                    'id_restaurante': session['user_logged']
+                }
+                request_microservicio_usuarios = requests.put('http://localhost:7071/api/HttpTrigger3', json=change_password)
+                if(request_microservicio_usuarios.status_code==200):
+                    flash('Su contraseña a sido actualizada exitosamente')
+                    context = {'status_code': 200}
+                    return render_template('cambiar-password.html', **context)
+                else:
+                    flash('Algo salio mal o las contraseñas no coinciden, vuelvalo a intentar')
+                    context = {'status_code': 500}
+                    return render_template('cambiar-password.html', **context)
+            except:
+                flash('Algo salio mal o las contraseñas no coinciden, vuelvalo a intentar')
+                context = {'status_code': 500}
+                return render_template('cambiar-password.html', **context)
+
+    return redirect (url_for('rutas_sesiones.login_restaurante'))
--- rutas_sesiones.py
+++ rutas_sesiones.py
@@ -1,15 +1,48 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, sessions, url_for, session
 import requests
 import random
 
-rutas_sesiones = Blueprint('rutas_sesiones', __name__, template_folder='templates', static_folder='static')
-
+rutas_sesiones = Blueprint('rutas_sesiones', __name__,
+                           template_folder='templates', static_folder='static')
 
 # Pagina de login a la plataforma de administracion de restaurantes
 
+
 @rutas_sesiones.route('/login-restaurante', methods=['GET', 'POST'])
 def login_restaurante():
-    return render_template('login-restaurante.html')
+    if not 'user_logged' in session:
+        if (request.method == 'GET'):
+            return render_template('login-restaurante.html')
+        elif(request.method == 'POST'):
+            try:
+                id_restaurante = request.form['id-restaurante']
+                user = {
+                    'id_restaurante': id_restaurante,
+                    'password': request.form['contraseña']
+                }
+                request_microservicio_usuarios = requests.get(
+                    'http://localhost:7071/api/HttpTrigger2', json=user)
+                if (request_microservicio_usuarios.status_code == 200):
+                    session['user_logged'] = id_restaurante
+                    return redirect(url_for('rutas_restaurante.admin_page'))
+                else:
+                    flash(f'Usuario o contraseña incorrecta, vuelvalo a intentar')
+                    context = {'status_code': 500}
+                    return render_template('login-restaurante.html', **context)
+            except Exception as e:
+                flash(f'Lo sentimos algo fallo, vuelvelo a intentar')
+                context = {'status_code': 500}
+                return render_template('login-restaurante.html', **context)
+    return redirect(url_for('rutas_restaurante.admin_page'))
+
+
+# Boton loggout
+
+@rutas_sesiones.route('/logout', methods=['GET', 'POST'])
+def loggout():
+    session.pop('user_logged', None)
+    return redirect(url_for('rutas_sesiones.login_restaurante'))
+
 
 # Pagina para crear nuevas cuentas de restaurantes
 
@@ -21,7 +54,7 @@ def crear_cuenta():
         try:
             id_restaurante = random.randint(10000, 99999)
             restaurante = {
-                "id_restaurante":id_restaurante,
+                "id_restaurante": id_restaurante,
                 "nombre_restaurante": request.form['nombre-restaurante'],
                 "especialidad": request.form['especialidad'],
                 "chef": request.form['chef'],
@@ -34,17 +67,19 @@ def crear_cuenta():
             }
             request_microservicio_usuarios = requests.post(
                 'http://localhost:7071/api/HttpTrigger1', json=usuario)
-            request_microservicio_restaurante = requests.post('http://localhost:7070/api/HttpTrigger1', json=restaurante)
+            request_microservicio_restaurante = requests.post(
+                'http://localhost:7070/api/HttpTrigger1', json=restaurante)
             if (request_microservicio_usuarios.status_code == 200
                     and request_microservicio_restaurante.status_code == 200):
-                flash(f'Cuenta creada con exito. Su ID de restaurante es {id_restaurante}, guardelo.')
-                context = {'status_code':200}
+                flash(
+                    f'Cuenta creada con exito. Su ID de restaurante es {id_restaurante}, guardelo.')
+                context = {'status_code': 200}
                 return render_template('crear-cuenta.html', **context)
             else:
                 flash(f'Lo sentimos algo fallo, vuelvelo a intentar')
-                context = {'status_code':500}
+                context = {'status_code': 500}
                 return render_template('crear-cuenta.html', **context)
         except Exception as e:
             flash(f'Lo sentimos algo fallo, vuelvelo a intentar')
-            context = {'status_code':500}
+            context = {'status_code': 500}
             return render_template('crear-cuenta.html', **context)
